acb5c40d1cd07b6f97dff1cf3786c109ae78cc9b
Gregor Tätzner <brummbq@gmail.com>
2016-10-27 22:39:19 +0200

    publish to hex

--- mix.exs
+++ mix.exs
@@ -1,36 +1,46 @@
 defmodule Cognixir.Mixfile do
-  use Mix.Project
-  use Mix.Config
+    use Mix.Project
+    use Mix.Config
 
-  def project do
-    [app: :Cognixir,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
+    def project do
+        [app: :Cognixir,
+         version: "0.1.0",
+         elixir: "~> 1.3",
+         description: description(),
+         package: package(),
+         deps: deps(),
+         build_embedded: Mix.env == :prod,
+         start_permanent: Mix.env == :prod,
+         deps: deps()]
+    end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger, :poison, :httpotion]]
-  end
+    # Configuration for the OTP application
+    #
+    # Type "mix help compile.app" for more information
+    def application do
+        [applications: [:logger, :poison, :httpotion]]
+    end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [
-        {:httpotion, "~> 3.0.2"},
-        {:poison, "~> 3.0"}
-    ]
-  end
+    defp deps do
+        [
+            {:httpotion, "~> 3.0.2"},
+            {:poison, "~> 3.0"},
+            {:ex_doc, "~> 0.11", only: :dev}
+        ]
+    end
+
+
+    defp description do
+        """
+        Elixir Client for Microsoft Cognitive Services. With this library you can access the cognitive services api and add machine intelligence to your elixir application!
+        """
+    end
+
+    defp package do
+         [name: :cognixir,
+         files: ["lib", "mix.exs", "README*", "LICENSE*"],
+         maintainers: ["Gregor Tätzner"],
+         licenses: ["MIT"],
+         links: %{"GitHub" => "https://github.com/BrummbQ/cognixir"}]
+    end
 end
